15abd4bf7081c2e24f9debcb47a12686409ab1b3
AshIdiot <saponaramatteo30@gmail.com>
2026-09-19 19:26:06 -0400

    Created Goblin Enemy script and IDamagable Interface

---
+++ src/Enemies/GoblinEnemy.java
@@ -0,0 +1,171 @@
+package Enemies;
+
+import Builders.FrameBuilder;
+import Engine.ImageLoader;
+import GameObject.Frame;
+import GameObject.ImageEffect;
+import GameObject.SpriteSheet;
+import Level.Enemy;
+import Level.MapEntity;
+import Level.Player;
+import Utils.AirGroundState;
+import Utils.Direction;
+import Utils.Point;
+import Interfaces.IDamagable;
+
+import java.util.HashMap;
+
+public class GoblinEnemy extends Enemy implements IDamagable{
+    private float gravity = .5f;
+    private float movementSpeed;
+    private float walkSpeed = .5f;
+    private float chaseSpeed = 1.5f; // Speed of Goblin when the player is in its aggro range
+    private Direction startFacingDirection;
+    private Direction facingDirection;
+    private AirGroundState airGroundState;
+    private float aggroRadius = 4f;
+    private boolean chasing = false;
+    private int health = 1;
+
+    public GoblinEnemy (Point location, Direction facingDirection) {
+        super(location.x, location.y, new SpriteSheet(ImageLoader.load("GoblinEnemy.png"), 24, 15), "WALK_LEFT");
+        this.startFacingDirection = facingDirection;
+        this.initialize();
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        facingDirection = startFacingDirection;
+        if (facingDirection == Direction.RIGHT) {
+            currentAnimationName = "WALK_RIGHT";
+        } else if (facingDirection == Direction.LEFT) {
+            currentAnimationName = "WALK_LEFT";
+        }
+        airGroundState = AirGroundState.GROUND;
+    }
+
+    @Override
+    public void update(Player player) {
+        float moveAmountX = 0;
+        float moveAmountY = 0;
+
+        // add gravity (if in air, this will cause bug to fall)
+        moveAmountY += gravity;
+
+        if (inRange(player)) {
+            chasing = true;
+            if (player.getX() > this.getX()) {
+                facingDirection = Direction.RIGHT;
+            }
+            else if (player.getX() <= this.getX()) {
+                facingDirection = Direction.LEFT;
+            }
+        }
+        else {
+            chasing = false;
+        }
+
+        // if on ground, walk forward based on facing direction
+        if (airGroundState == AirGroundState.GROUND) {
+            if (chasing) {
+                movementSpeed = chaseSpeed;
+            }
+            else {
+                movementSpeed = walkSpeed;
+            }
+            if (facingDirection == Direction.RIGHT) {
+                moveAmountX += movementSpeed;
+            } else {
+                moveAmountX -= movementSpeed;
+            }
+        }
+
+        // move bug
+        moveYHandleCollision(moveAmountY);
+        moveXHandleCollision(moveAmountX);
+
+        super.update(player);
+    }
+
+    @Override
+    public void onEndCollisionCheckX(boolean hasCollided, Direction direction,  MapEntity entityCollidedWith) {
+        // if bug has collided into something while walking forward,
+        // it turns around (changes facing direction)
+        if (hasCollided) {
+            if (direction == Direction.RIGHT) {
+                facingDirection = Direction.LEFT;
+                currentAnimationName = "WALK_LEFT";
+            } else {
+                facingDirection = Direction.RIGHT;
+                currentAnimationName = "WALK_RIGHT";
+            }
+        }
+    }
+
+    @Override
+    public void onEndCollisionCheckY(boolean hasCollided, Direction direction, MapEntity entityCollidedWith) {
+        // if bug is colliding with the ground, change its air ground state to GROUND
+        // if it is not colliding with the ground, it means that it's currently in the air, so its air ground state is changed to AIR
+        if (direction == Direction.DOWN) {
+            if (hasCollided) {
+                airGroundState = AirGroundState.GROUND;
+            } else {
+                airGroundState = AirGroundState.AIR;
+            }
+        }
+    }
+
+    @Override
+    public HashMap<String, Frame[]> loadAnimations(SpriteSheet spriteSheet) {
+        return new HashMap<String, Frame[]>() {{
+            put("WALK_LEFT", new Frame[] {
+                    new FrameBuilder(spriteSheet.getSprite(0, 0), 8)
+                            .withScale(2)
+                            .withBounds(6, 6, 12, 7)
+                            .build(),
+                    new FrameBuilder(spriteSheet.getSprite(0, 1), 8)
+                            .withScale(2)
+                            .withBounds(6, 6, 12, 7)
+                            .build()
+            });
+
+            put("WALK_RIGHT", new Frame[] {
+                    new FrameBuilder(spriteSheet.getSprite(0, 0), 8)
+                            .withScale(2)
+                            .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+                            .withBounds(6, 6, 12, 7)
+                            .build(),
+                    new FrameBuilder(spriteSheet.getSprite(0, 1), 8)
+                            .withScale(2)
+                            .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+                            .withBounds(6, 6, 12, 7)
+                            .build()
+            });
+        }};
+    }
+
+    public int getHealth() {
+        return health;
+    }
+    
+    public void takeDamage(int damage) {
+        health -= damage;
+        if (health < 1) {
+            die()
+        }
+    }
+
+    public void die() {
+        
+    }
+
+    private boolean inRange(Player player) {
+        float pX = player.getX();
+        float pY = player.getY();
+        if (Math.pow((pX - this.getX()),2) + Math.pow((pY - this.getY()),2) <= Math.pow(aggroRadius,2)) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/Interfaces/IDamagable.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+public interface IDamagable {
+    public int getHealth();
+    public void takeDamage(int damage);
+    public void die();
+}
